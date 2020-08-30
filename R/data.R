a7596d322245a0bfa7af1f2b1654bab56ab828d2
Yi Feng <yifeng94@umd.edu>
2020-08-29 21:09:25 -0400

    meddata

--- R/data.R
+++ R/data.R
@@ -8,3 +8,10 @@ NULL
 #' A data set containing the data from female turtles.
 #' @format an rda file
 "turtle"
+
+
+#' "med.data"
+#'
+#' A data set containing the data from 61 fourth year medical students (Marascuilo & Levin, 1983).
+#' @format an rda file
+"med.data"
