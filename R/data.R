7aee98004f1d0f4231704d8d20e1dc767681b535
Yi Feng <yifeng94@umd.edu>
2020-08-30 22:09:02 -0400

    update datasets

--- R/data.R
+++ R/data.R
@@ -3,15 +3,72 @@
 #' @name EDMS657Data
 NULL
 
-#' "turtle"
+#' female turtles data.
 #'
 #' A data set containing the data from female turtles.
 #' @format an rda file
 "turtle"
 
 
-#' "med.data"
+#' Data for medical students.
 #'
 #' A data set containing the data from 61 fourth year medical students (Marascuilo & Levin, 1983).
 #' @format an rda file
 "med.data"
+
+
+#' Spearman's data.
+#'
+#' The correlation matrix for Spearman's data. 
+#' @format an rda file
+"spearman"
+
+
+#' Hotelling's data.
+#'
+#' The correlation matrix for hotelling's data (1933). It is a correlation matrix from a sample of 200 seventh grade students on tests of 1) reading speed, 2) reading power, 3) arithmetic speed, and 4) arithmetic power.
+#' @format an rda file
+"hotelling"
+
+
+#' Thurstone's box problem.
+#'
+#' The correlation matrix for Thurstone's box problem.
+#' @format an rda file
+"box.data"
+
+
+#' Assessment data from Hancock (1994).
+#'
+#' The correlation matrix reported in Hancock (1994). The examinations for two educational measurement/statistics classes were constructed as half multiple-choice (MC) and half constructed-response (CR), containing equal number of items in each format written for the knowledge (know), comprehension (comp), application (appl), and analysis (anal) levels of Bloom’s Taxonomy.
+#' @format an rda file
+"test.data"
+
+
+
+#' Polytomous item response data from Finney et al. (2004).
+#'
+#' The item response data from Finney et al. (2004). There were four scale items that are expected to measure a single factor: Working Avoidance Orientation. The responses were coded on a 3-point scale (1 = not true, 3 = very true), n=300. 
+#' @format an rda file
+"wav"
+
+
+#' Distance between US cities.
+#'
+#' The distance matrix between 11 US cities. 
+#' @format an rda file
+"geodist"
+
+
+#' Rate how similar are the nine types of animals.
+#'
+#' Someone(presumably Greg) has ranked all the possible pairs of the 9 animals in terms of their similarity. We want to know what governs this individual’s opinions about the similarity/dissimilarity between the animals.
+#' @format an rda file
+"animal"
+
+
+#' Classifying beers.
+#'
+#' A marketing analyst is trying to develop a classification scheme for beer based on several characteristics that may be relevant to consumers. She samples 20 popular beers and gathers data on their per-serving in terms of the following 4 aspects: calorie content, sodium content, alcohol content, and cost per unit volume.
+#' @format an rda file
+"beer"
