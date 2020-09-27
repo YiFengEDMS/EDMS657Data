18328fe9e7db7e2ee603a7f11bf655414c106863
Yi Feng <yifeng94@umd.edu>
2020-09-26 21:21:22 -0400

    Manova

--- R/data.R
+++ R/data.R
@@ -41,6 +41,7 @@ NULL
 #' Assessment data from Hancock (1994).
 #'
 #' The correlation matrix reported in Hancock (1994). The examinations for two educational measurement/statistics classes were constructed as half multiple-choice (MC) and half constructed-response (CR), containing equal number of items in each format written for the knowledge (know), comprehension (comp), application (appl), and analysis (anal) levels of Bloom’s Taxonomy.
+#' @references Hancock (1994)
 #' @format an rda file
 "test.data"
 
@@ -72,3 +73,40 @@ NULL
 #' A marketing analyst is trying to develop a classification scheme for beer based on several characteristics that may be relevant to consumers. She samples 20 popular beers and gathers data on their per-serving in terms of the following 4 aspects: calorie content, sodium content, alcohol content, and cost per unit volume.
 #' @format an rda file
 "beer"
+
+
+
+#' Data used for two-group MANOVA (Hotelling's T^2).
+#'
+#' Middle school students are randomly assigned to an educational treatment or a control group. After receiving their treatment or control experience, students are evaluated in terms of cognitive and affective outcomes together. We are interested in testing whether there is any difference between the treatment/control groups regarding the set of outcomes.
+#' @format an rda file
+"Hotellings_T"
+
+
+
+#' Data used for three-group MANOVA.
+#'
+#' Middle school students are randomly assigned to one of three treatment groups. After receiving their respective treatment or control experience, students are evaluated in terms of cognitive and affective outcomes together. We are interested in testing whether there is any difference between the three treatment groups regarding the set of outcomes.
+#' @format an rda file
+"manova_dat"
+
+
+#' Data used for three-group MANOVA with six outcomes.
+#'
+#' The faculties from a public research univeristy were evaluated at the end of a semester with regard to the courses they taught in that semester. Students rated the faculties on six aspects. We are interested in testing whether there is any difference between different types of faculties regarding the set of outcomes.
+#' @format an rda file
+"Teacher_ratings"
+
+
+#' Data used for two-way MANOVA.
+#'
+#' Researchers are interested in studying the effects of drugs and physical exercise on people’s weight loss and positive attitude. They wish to test both the main effects and interactions effects.
+#' @format an rda file
+"MANOVA2way"
+
+
+#' Data used for MANOVA with repeated measures.
+#'
+#' Researchers are interested in studying whether the brand of sneakers affects how high individuals can jump up from the ground. A group of subjects were tested repeatedly wearing three different brands of sneakers: Reebok, Nike, and Converse. 
+#' @format an rda file
+"MANOVA_RM"
